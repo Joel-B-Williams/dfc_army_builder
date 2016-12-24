a30d9e8ca8a8d8c552ac2a6eaefcf4f523ec39ab
Joel Williams <joel.b.williams.is@gmail.com>
2016-12-23 21:05:27 -0600

    Add linked table of group sizes

--- ships.rb
+++ ships.rb
@@ -2,11 +2,12 @@ require_relative 'roster'
 require_relative 'tables'
 require 'sqlite3'
 
-Tables.tables(Tables::DB)
+db = Tables::DB
+Tables.tables(db)
 
 class Ship
 
-	def initialize(faction_roster, roster_index)
+	def initialize(faction_roster, roster_index, db)
 		@name = faction_roster[roster_index][0]
 		@scan = faction_roster[roster_index][1]
 		@signature = faction_roster[roster_index][2]
@@ -18,7 +19,7 @@ class Ship
 		@tonnage = faction_roster[roster_index][8]
 		@special = faction_roster[roster_index][9]
 		@points = faction_roster[roster_index][10]
-		@db = Tables::DB
+		@db = db
 		save
 	end
 
@@ -26,21 +27,24 @@ class Ship
 
 	def save
 		populate_ships = '
-		INSERT INTO ships (name, scan, signature, thrust, hull, armor, point_defense, group_size, tonnage, special, points) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
+		INSERT INTO ships (name, scan, signature, thrust, hull, armor, point_defense, group_sizes_id, tonnage, special, points) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
 			@db.execute(populate_ships, [@name, @scan, @signature, @thrust, @hull, @armor, @point_defense, @group_size, @tonnage, @special, @points])
 	end
 
 
 end
 
-	def create_ships(faction_roster)
+# Move to driver code
+	def create_ships(faction_roster, db)
 		created_ships = []
 		faction_roster.each_with_index do |ship, index|
 			created_ship = []
-			created_ship << ship = Ship.new(faction_roster, index)
+			created_ship << ship = Ship.new(faction_roster, index, db)
 			created_ships << created_ship
 		end
 		created_ships
 	end
 
-ucm_ships = create_ships(Roster::UCM_ROSTER)
+Tables.populate_group_sizes(db)
+# Conditional on choosing UCM as faction
+ucm_ships = create_ships(Roster::UCM_ROSTER, db)
--- tables.rb
+++ tables.rb
@@ -6,6 +6,11 @@ module Tables
 	
 	def Tables.tables(db)
 		create_tables = '
+		CREATE TABLE IF NOT EXISTS group_sizes(
+		id INTEGER PRIMARY KEY,
+		group_size TEXT
+		);
+
 		CREATE TABLE IF NOT EXISTS ships (
 		id INTEGER PRIMARY KEY,
 		name TEXT,
@@ -15,12 +20,18 @@ module Tables
 		hull INTEGER,
 		armor TEXT,
 		point_defense INTEGER,
-		group_size INTEGER,
+		group_sizes_id INTEGER REFERENCES group_sizes(id),
 		tonnage TEXT,
 		special TEXT,
 		points INTEGER
 		);'
-		db.execute(create_tables)
+		db.execute_batch(create_tables)
+	end
+
+	def Tables.populate_group_sizes(db)
+	sizes = ["1", "1-2", "1-3", "1-4", "2-3", "2-4", "2-6"]
+	add_group_size = 'INSERT INTO group_sizes (group_size) VALUES (?)'
+	sizes.each {|size| db.execute(add_group_size, [size])}
 	end
 
 end
